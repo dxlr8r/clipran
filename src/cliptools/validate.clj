ad0d53867d70754fe8498bfe70c5c8c4440e4282
Simen Strange Øya <simen@ifixit.no>
2015-11-19 18:13:30 +0100

    Updated docstrings and better parameter names

--- src/cliptools/validate.clj
+++ src/cliptools/validate.clj
@@ -3,27 +3,27 @@
 
 (def ^:private ip-pattern #"\d+\.\d+\.\d+\.\d+")
 
-(defn ip? [arg]
-  (if (re-matches ip-pattern arg)
-    (= 4 (count (take-while #(> 256 %) (split-ip arg))))
+(defn ip? [ip]
+  (if (re-matches ip-pattern ip)
+    (= 4 (count (take-while #(> 256 %) (split-ip ip))))
     false))
 
-(defn ip-range? [from to]
+(defn ip-range? "Is 'from' and 'to' a range?" [from to]
   (and
    (ip? from)
    (ip? to)
    (< (ip->int from) (ip->int to))))
 
-(defn range-str? [arg]
+(defn range-str? "Is string a range?" [rng-str]
   (let [range-pattern (re-pattern (str ip-pattern "-" ip-pattern))]
-    (if (re-matches range-pattern arg) true false)))
+    (if (re-matches range-pattern rng-str) true false)))
 
-(defn ip-range-str? [arg]
-  (if (range-str? arg) (apply ip-range? (split-range arg)) false))
+(defn ip-range-str? "Is string a valid IP range?" [ip-rng-str]
+  (if (range-str? ip-rng-str) (apply ip-range? (split-range ip-rng-str)) false))
 
-(defn ip-address-or-range? [arg]
+(defn ip-address-or-range? "Is argument an IP address or an IP range?" [arg]
   (if (ip? arg)
     "address"
-    (if (ip-range? arg)
+    (if (ip-range-str? arg)
       "range"
       false)))
